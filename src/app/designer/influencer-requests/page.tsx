4e39b90fed319a74df9c048a2340ce8186415377
Vicorico17 <vic.cazacu@gmail.com>
2025-06-19 02:18:37 +0300

    fix: mark designer influencer requests page as client component

--- src/app/designer/influencer-requests/page.tsx
+++ src/app/designer/influencer-requests/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useEffect, useState } from 'react';
 import { useDesignerAuth } from '@/contexts/DesignerAuthContext';
 import { User, Package, CheckCircle, XCircle } from 'lucide-react';
